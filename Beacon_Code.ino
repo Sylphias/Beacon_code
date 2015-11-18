4c21902156f2dc06cdf2c86315d16cf75f2d1052
Sylphias <sylpharyon@gmail.com>
2015-11-19 02:10:03 +0800

    Variable Payloads

--- Beacon_Code.ino
+++ Beacon_Code.ino
@@ -1,5 +1,6 @@
 #include <XBee.h>
 #include <SoftwareSerial.h>
+//#include <string.h>
 //#include <LiquidCrystal.h>
  
 //LiquidCrystal lcd(12,11,5,4,3,2);
@@ -7,13 +8,12 @@
 // XBee's DOUT (TX) is connected to pin 8 (Arduino's Software RX)
 // XBee's DIN (RX) is connected to pin 9 (Arduino's Software TX)
 SoftwareSerial serial1(0, 1); // RX, TX
- 
+
 XBee xbee=XBee();
 XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
 Rx64Response rx64 = Rx64Response();
-uint8_t payload[10] ; 
-Tx16Request tx = Tx16Request(0x0000, payload, sizeof(payload));
+
 uint8_t option = 0;
 uint8_t data = 0;
 uint8_t rssi = 0;
@@ -23,12 +23,21 @@ void setup()
   Serial.begin(9600);
   serial1.begin(9600);
   xbee.setSerial(Serial);
-//  lcd.begin(16,2);
-//  lcd.clear();
 }
  
 void loop() 
 {
+  String test_msg = "this is a string message";
+  char payload[64] ;
+  test_msg.toCharArray(payload, test_msg.length());
+//  payload = format_message_payload(1,1,1);
+    uint8_t payload_bytes[64] = {};
+//  for(int char_index = 0 ; char_index < sizeof(payload); char_index++){
+//    payload_bytes[char_index] = payload[char_index];
+//  }
+  memcpy((uint8_t*)payload_bytes,(char*)payload, 64);
+//  Serial.println((char)payload_bytes[1]);
+  Tx16Request tx = Tx16Request(0x0000, payload_bytes, sizeof(payload_bytes));
   xbee.readPacket(100);
   if (xbee.getResponse().isAvailable())
   {
@@ -40,8 +49,7 @@ void loop()
         xbee.getResponse().getRx16Response(rx16);
         rssi = rx16.getRssi();
 
-//        lcd.clear();
-//        lcd.print(rssi);
+
         Serial.println(rssi);
         String ab ;
         for(int x = 0 ; x< rx16.getDataLength()+1; x++){
@@ -49,21 +57,46 @@ void loop()
            ab = ab + y;
         }
         Serial.println(ab);
-//       Serial.println(rx16.getFrameData());
 
         xbee.send(tx);
-        //Serial.print('sent');
       } 
       else 
       {
         Serial.println("64");
         xbee.getResponse().getRx64Response(rx64);
         rssi = rx64.getRssi();
-//        lcd.clear();
-//        lcd.print(rssi);
         Serial.println(rssi);
-//        xbee.send(rssi);
       }
     }
   }
 }
+// Establishing Message Structure
+// Edit This code to change what is sent in payload
+// Message Payload can only contain 256 Bytes, First few of which are reserved for key information
+// 1) Message Type
+// 2) Hop Number
+// 3) Message ID
+
+//char format_message_payload(char message_type, char hop_number, char message_id)
+//{
+//  char payload[256] ={message_type, hop_number , message_id};
+//  char test_string[256] = 'Help!, Help me! I am trapped!!!';
+//  int i = 3;
+//  for (int x = 0; x < sizeof(test_string); x++){
+//    Serial.print(test_string[x]);
+//    payload[i] = test_string[x];
+//    i = i+1;
+//  }
+//  return payload;
+//}
+
+
+
+
+
+
+
+
+
+
+
