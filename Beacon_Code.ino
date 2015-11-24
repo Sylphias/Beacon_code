673acdae3f55a613461660429e0ce0db32856d21
Sylphias <sylpharyon@gmail.com>
2015-11-25 01:18:27 +0800

    Added A switch statement to filter packet headers

--- Beacon_Code.ino
+++ Beacon_Code.ino
@@ -2,38 +2,52 @@
 #include <SoftwareSerial.h>
  
  
-// XBee's DOUT (TX) is connected to pin 8 (Arduino's Software RX)
+// XBee's DOUT (TX) is connected to pin 1 (Arduino's Software RX)
 // XBee's DIN (RX) is connected to pin 9 (Arduino's Software TX)
 
 // Establishing Message Structure
 // Edit This code to change what is sent in payload
 // Message Payload can only contain 256 Bytes, First few of which are reserved for key information
-// 1) Message Type (1,2,3) 1 - broadcast 2 - play help 3 - redirect but dont play
-// 2) Hop Number (In hexadecimal)
-// 4) Message ID
+// M) Message Type (1,2,3) 1 - broadcast 2 - play help 3 - redirect but dont play
+// H) Hop Number (In hexadecimal)
+// I) Message ID
+// R) Receiver Chains (Ie addresses of the chips the message has passed through)
+// E) End Packet
 // Look at CRC Encoding
+//
 SoftwareSerial serial1(0, 1); // RX, TX
 
 XBee xbee=XBee();
 XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
 Rx64Response rx64 = Rx64Response();
-
+const int beacon_id = 1 ;
 uint8_t option = 0;
 uint8_t data = 0;
 uint8_t rssi = 0;
 
+int old_message_id = 0;
+int original_beacon_ID = 0;
 
+//Beacon mode tells us if we're listening for microphone(1), or broadcasting(0)
+int beacon_mode = 0;
 
 void setup() 
 {
   Serial.begin(9600);
   serial1.begin(9600);
   xbee.setSerial(Serial);
+  pinMode(13,INPUT);
+  pinMode(12,OUTPUT);
+  
 }
  
 void loop() 
 {
+  int message_type = 0;
+  int hop_number = 0;
+  int message_id = 0;
+  String beacon_chain;
   xbee.readPacket(100);
   if (xbee.getResponse().isAvailable())
   {
@@ -45,27 +59,65 @@ void loop()
         xbee.getResponse().getRx16Response(rx16);
         rssi = rx16.getRssi();
         Serial.println(rssi);
-        //Convert Unsigned Char to Integers
-        int message_type = (char)rx16.getData(0)-'0';
-        int hop_number = (char)rx16.getData(1)-'0';
-        int message_id = (char)rx16.getData(2)-'0';
-        Serial.println(message_type);
-        // Handling various types of messages 
-        switch(message_type){
-          case 1:
-            format_message_payload(1,hop_number,message_id);
-          break;
-          case 2:
-            //todo
-          break;
-          case 3:
-            //todo
-          break;
-          default:
-            //todo
-          break;
+
+        // This look goes through the packets to extract the various data
+        for(int packet_counter = 0 ; packet_counter < rx16.getDataLength; packet_counter++)
+        {
+          char packet_sec = (char)rx16.getData(packet_counter)-'0';
+          // Arduino has no regex... have to do it the dirty way
+          // Apparently, there is a function in C called sscanf
+          switch (packet_sec)
+          {
+            case 'M':
+              //if (packet_sec){
+              int m_section_counter = packet_counter;
+              char section_char;
+              do
+              {
+                section_char = (char)rx16.getData(packet_counter)-'0';
+                m_section_counter += 1;
+              }
+              while(section_char != 'H')
+              //}
+            break;
+            case 'H':
+            break;
+            case 'I':
+            break;
+            case 'R':
+            break;
+            case 'E':
+            break;
+          }
         }
 
+
+        //Convert Unsigned Char to Integers
+        message_type = (char)rx16.getData(0)-'0';
+        hop_number = (char)rx16.getData(1)-'0';
+        message_id = (char)rx16.getData(5)-'0';
+        beacon_chain = String((char) rx16.getData(10)-'0');
+      
+        // Handling various types of messages 
+        // Check if you re-received the previous message
+        if(old_message_id != message_id && original_beacon_ID != beacon_chain[1]){
+          switch(message_type){
+            case 1:
+              Serial.println('Test');
+              beacon_mode = 1;
+              //format_message_payload(message_type,hop_number,message_id, beacon_chain);
+            break;
+            case 2:
+              //todo
+            break;
+            case 3:
+              //todo
+            break;
+            default:
+              //todo
+            break;
+          }
+        }
       } 
       else 
       {
@@ -76,23 +128,29 @@ void loop()
       }
     }
   }
+  if(digitalRead(13) == HIGH && beacon_mode == 1)
+  {
+    digitalWrite(12, HIGH);
+    format_message_payload(message_type,hop_number,message_id, beacon_chain);;
+  }
+  else
+  {
+    digitalWrite(12,LOW);
+  }
 }
 
-void format_message_payload(int message_type, int hop_number, int message_id)
+//Prepares the outgoing message to be sent
+void format_message_payload(int message_type, int hop_number, int message_id,String beacon_chain)
 {
   String padded_hop_number;
   hop_number++;
   if(hop_number < 10)
   {
     padded_hop_number = "0"+hop_number;
-  }
-  message_type = 2;
-  message_id = 4;
-  Serial.print("Padded:");
-  Serial.println(hop_number);
-  String composed_message = String(message_type) + String(hop_number) + String(message_id) + "TestPad";
+  } 
+  beacon_chain += beacon_id;
+  String composed_message = String(message_type) + String(hop_number) + String(message_id) + beacon_chain;
   char payload[32];
-  Serial.println(composed_message); 
   composed_message.toCharArray(payload, composed_message.length()+1);
   Serial.println(payload);
   uint8_t payload_bytes[32] = {};
@@ -101,4 +159,10 @@ void format_message_payload(int message_type, int hop_number, int message_id)
   xbee.send(tx);
 
 }
+
+
+
+
+
+
 
